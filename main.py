3b0455c0ecdd5ebb643662c39e51a2aa737df31d
Shanjjeev-Lab <shanjjeevrohan2911@gmail.com>
2024-09-25 18:37:55 +0530

    Python Data Structure Activity

---
+++ main.py
@@ -0,0 +1,27 @@
+alist = ["Student 1", 25, "9thGrade", 98]
+print(alist)
+
+fruits = ["Apple", "Banana", "Mango", "BlueBerry", "BlackBerry"]
+
+print("The First fruit:", fruits[0])
+print("The Third fruit:", fruits[2])
+print("The last fruit:", fruits[4])
+print("The list Size:", len(fruits))
+
+fruits.append("Orange")
+print("After adding:",fruits)
+
+fruits.sort()
+print("Arranging the fruits in ascending order:", fruits)
+
+fruits.reverse()
+print("Arranging the fruits in descending order:", fruits)
+
+print("Multiply list element:", fruits * 2)
+
+print("Slicing:", fruits[:6])
+
+fruits.clear()
+print("Clearing the fruits:", fruits)
+              
+              
